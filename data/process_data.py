718927c3dac75f94e79c0ac72587eea61a3f76cb
vasthav <sreevasthavan@gmail.com>
2020-05-09 20:48:57 +0530

    feat: added cleaning and ml scripts

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,78 @@
+# import libraries
 import sys
+
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    messages.head()
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    categories.head()
+
+    # merge datasets
+    df = messages.merge(categories, on='id', how='inner')
+    df.head()
+
+    return df
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(";", expand=True)
+    categories.head()
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[1]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = list([x[:-2] for x in row])
+    # print(category_colnames)
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    # categories.head()
+
+    import copy
+    c = copy.deepcopy(categories)
+    for column in categories:
+        # set each value to be the last character of the string
+        #     print(categories[column])
+        categories[column] = categories[column].apply(lambda x: x[-1])
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    # categories.head()
+
+    categories['related'] = categories['related'].apply(lambda x: 1 if x not in ['1', '0', 1, 0] else x)
+
+    # drop the original categories column from `df`
+    df.drop(columns='categories', inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # check number of duplicates
+    # df.duplicated().sum()
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    # check number of duplicates
+    # df.duplicated().sum()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('cleaned_data', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -24,18 +86,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
               'DisasterResponse.db')
 
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,127 @@
+import re
+import ssl
 import sys
+
+import nltk
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+
+# running nltk download gave ssl error.
+# Resolution reference: https://github.com/gunthercox/ChatterBot/issues/930#issuecomment-322111087
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
 
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('cleaned_data', engine)
+
+    # Clean dataframe remove na values
+    df.dropna(axis='index', inplace=True)
+
+    # Preparing data
+    X = df.message.values
+    t = df.drop(columns=['id', 'message', 'original', 'genre']).astype(int)
+
+    X = df['message']
+    Y = t
+
+    return X, Y, list(Y.columns)
 
 
 def tokenize(text):
-    pass
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    tokens = word_tokenize(text)
+
+    stop_word = stopwords.words("english")
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+
+    lemmed = [lemmatizer.lemmatize(w) for w in tokens if w not in stop_word]
+
+    return lemmed
 
 
 def build_model():
-    pass
+    from sklearn.multioutput import MultiOutputClassifier
+    from sklearn.pipeline import Pipeline
+    from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import GridSearchCV
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=10, n_jobs=-1)))
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'tfidf__use_idf': (True, False),
+        'vect__max_features': (None, 5000),
+        'clf__estimator__n_estimators': [10, 20]
+    }
+
+    # Grid Search best parameters after running with above parameters.
+    # parameters = {
+    #     'vect__max_features': (None, 5000),
+    #     'vect__ngram_range': (1, 2),
+    #     'vect__max_df': (1.0),
+    #     'tfidf__use_idf': (True),
+    #     'clf__estimator__n_estimators': [20],
+    # }
+
+    # Testing parameters
+    # parameters = {
+    #     'vect__ngram_range': [(1, 1)],
+    #     'clf__estimator__n_estimators': [10, 20],
+    # }
+
+    cv = GridSearchCV(estimator=pipeline, param_grid=parameters, scoring='f1_macro', cv=None, n_jobs=-1, verbose=10)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    # references:
+    # https://stackoverflow.com/questions/38697982/python-scikit-learn-multi-class-multi-label-performance-metrics
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html?highlight=report#sklearn.metrics.classification_report
+
+    # predict on test data
+    predicted = model.predict(X_test)
+
+    from sklearn.metrics import classification_report
+
+    y_pred_pd = pd.DataFrame(predicted, columns=Y_test.columns)
+    for column in Y_test.columns:
+        print(f'Class: {column}\n')
+        print(classification_report(Y_test[column], y_pred_pd[column]))
+
+    # Finding the Accuracy
+    accuracy = (predicted == Y_test).mean()
+    print(f'accuracy = {accuracy.mean()} \n')
 
 
 def save_model(model, model_filepath):
-    pass
+    import pickle
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -27,13 +130,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -43,10 +146,10 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
+              'train_classifier.py ../data/DisasterResponse.db classifier_simple.pkl')
 
 
 if __name__ == '__main__':
